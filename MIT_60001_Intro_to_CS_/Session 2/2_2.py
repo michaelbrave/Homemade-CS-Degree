654414edd13c1df8aec8391b942f11e7eb59e106
michaelbrave <michaelbrave@outlook.com>
2020-02-02 22:11:50 -0800

    fixed 2_2

--- MIT_60001_Intro_to_CS_/Session 2/2_2.py
+++ MIT_60001_Intro_to_CS_/Session 2/2_2.py
@@ -1,31 +1,12 @@
 # Finger exercise: Write a program that examines three variables—x, y, and z—and prints the largest odd number among them. If none of them are odd, it should print a message to that effect.
 
-x,y,z = 4,1,6
+x,y,z = 5,21,7
 
-if x % 2 == 0:
-    if y % 2 == 0:
-        if z % 2 == 0:
-            print ("None of them are odd")
-        else:
-            print (z)
-    elif y > z or z % 2 == 0:
-        print (y)
-    else:
-        print (z)
-elif y % 2 == 0:
-    if z % 2 == 0 or x > z:
-        print (x)
-    else:
-        print (z)
-elif z % 2 == 0:
-    if x > y:
-        print (x)
-    else:
-        print (y)
-else:
-    if x > y and x > z:
-        print (x)
-    elif y > z:
-        print (y)
-    else:
-        print (z)
+if (x % 2 == 0) and (y % 2 == 0) and (y % 2 == 0):
+    print("None of them are odd")
+elif (x % 2 != 0) and (((x > y) and (y % 2 != 0)) or y % 2 == 0) and (((x > z) and (z % 2 != 0)) or z % 2 == 0):
+    print (x)
+elif (y % 2 != 0) and (((y > x) and (x % 2 != 0)) or x % 2 == 0) and (((y > z) and (z % 2 != 0)) or z % 2 == 0):
+    print (y)
+elif (z % 2 != 0) and (((z > x) and (y % 2 != 0)) or y % 2 == 0) and (((z > x) and (x % 2 != 0)) or x % 2 == 0):
+    print (z)
