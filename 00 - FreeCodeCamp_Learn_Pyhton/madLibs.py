d14447db6d4be5e3fddf0618453419cd872a377f
michaelbrave <michaelbrave@outlook.com>
2020-02-17 14:50:35 -0800

    madlibs

---
+++ 00 - FreeCodeCamp_Learn_Pyhton/madLibs.py
@@ -0,0 +1,9 @@
+color = input ("Enter a color: ")
+plural = input ("Enter a plural noun: ")
+celebrity = input ("Enter a Celebrity Name: ")
+
+print("Roses are " + color)
+print(plural + " are blue")
+print("I love " + celebrity)
+
+
