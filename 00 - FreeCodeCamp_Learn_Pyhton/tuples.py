8751c64e95ef439326e086566f81a2ec267b4e5b
michaelbrave <michaelbrave@outlook.com>
2020-02-17 18:49:00 -0800

    tuples 2

--- 00 - FreeCodeCamp_Learn_Pyhton/tuples.py
+++ 00 - FreeCodeCamp_Learn_Pyhton/tuples.py
@@ -1,5 +1,6 @@
 coordinates = (4, 5)
 #tuples are immutable, can't be changed or modified
+#list of tuples = [(4, 5), (9, 7)]
 print(coordinates[0])
 #usually you will use it for something that doesn't need to change
 
