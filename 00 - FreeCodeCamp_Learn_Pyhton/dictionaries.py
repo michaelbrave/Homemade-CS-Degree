32b298620f9b99dcac2bea57506f1e3ffd0b4c63
michaelbrave <michaelbrave@outlook.com>
2020-02-19 18:00:17 -0800

    dictionaries

---
+++ 00 - FreeCodeCamp_Learn_Pyhton/dictionaries.py
@@ -0,0 +1,19 @@
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+print(monthConversions["Nov"])
+#print(monthConversions.get("Dec"))
+#lets you specify a key that may not be found
+#keys need to be unique, but otherwise can be almost anything numbers or whatever
