fda1a06b341b84f0824d39870a338d7d6e652c5b
michaelbrave <michaelbrave@outlook.com>
2020-02-19 17:47:14 -0800

    more adv calc

---
+++ 00 - FreeCodeCamp_Learn_Pyhton/advCalc.py
@@ -0,0 +1,15 @@
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator: ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("that wasn't a proper operator")
