c12dcb42c725c4514e7a1a50594cfe9a59a7c06e
michaelbrave <michaelbrave@outlook.com>
2020-02-21 14:48:13 -0800

    letter replace improved slightly

--- 00 - FreeCodeCamp_Learn_Pyhton/translator.py
+++ 00 - FreeCodeCamp_Learn_Pyhton/translator.py
@@ -1,10 +1,13 @@
 def translate(phrase):
     translation = ""
     for letter in phrase:
-        if letter in "AEIOUaeiou":
-            translation = translation + "b"
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "B"
+            else:
+                translation = translation + "b"
         else:
             translation = translation + letter
     return translation
 
-print(translate(input("Enter a phrase")))
+print(translate(input("Enter a phrase: ")))
