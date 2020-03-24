36f08d648e660da566459440b3391d58b189b982
michaelbrave <michaelbrave@outlook.com>
2020-03-23 17:56:57 -0700

    part 3

---
+++ Swift_Codecademy/03-DogYears/DogYears.swift
@@ -0,0 +1,6 @@
+var dogAge: Int = 4
+var earlyYears: Int = 21 //first two human years make them the age of 21
+var laterYears: Int = (dogAge - 2) * 4
+var humanYears: Int = earlyYears + laterYears
+
+print("Puppy is \(humanYears) old in human years")
