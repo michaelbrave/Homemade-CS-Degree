03bc60323daa65e8a92a3eb5cef1deb9e6b8667a
Mike Brave <michaelbrave@outlook.com>
2020-07-03 14:55:18 -0700

    7/3/20

---
+++ CPP_Cherno/HelloWorld/HelloWorld/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+void log(const char* message);
+
+int main()
+{
+	log("Hello There");
+	std::cin.get();
+}
---
+++ CPP_Cherno/README.md
@@ -0,0 +1,2 @@
+tutorial found at https://www.youtube.com/playlist?list=PLlrATfBNZ98dudnM48yfGUldqGD0S4FFb
+
