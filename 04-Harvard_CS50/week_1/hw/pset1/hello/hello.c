fc35f727eba3a288d5bb09a873274c9d2f37fcef
Mike Brave <mike@MacBook-Air.local>
2020-08-22 16:34:52 -0700

    week 2 1/4 through

--- 04-Harvard_CS50/week_1/hw/pset1/hello/hello.c
+++ 04-Harvard_CS50/week_1/hw/pset1/hello/hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include "../../../../lib/cs50.h"
+
+string name = get_string("What is your name?\n");
 
 int main(void)
 {
-	printf("hello, world\n");
+	printf("hello, %s\n", name);
 }
